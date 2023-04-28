246855d1fa2b8285bef261b3b68c6741b8069f4f
devcom33 <mouad.elb06@gmail.com>
2023-04-28 08:54:39 -0700

    _printf.c

---
+++ _printf.c
@@ -0,0 +1,52 @@
+#include "main.h"
+/**
+ * _printf - print function
+ * @format: format argument
+ */
+int _printf(const char *format, ...)
+{
+	va_list arguments;
+	va_start(arguments, format);
+	int cnt = 0;
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			++format;
+			if (*format == '%')
+			{
+				_putchar('%');
+				cnt++;
+			}
+			else if (*format == 's')
+			{
+				char *s = va_arg(arguments, char*);
+				for (i = 0; *s != '\0'; i++)
+				{
+					_putchar(*s);
+					s++;
+					cnt++;
+				}
+			
+			}
+			else if (*format == 'c')
+			{
+				int ctr = va_arg(arguments, int);
+				_putchar(ctr);
+				cnt++;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			cnt++;
+		}
+		format++;	
+	}
+
+
+
+
+
+}
