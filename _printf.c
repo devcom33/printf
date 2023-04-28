5c0fc9081bc73a2cd6e08223311641eae775352b
devcom33 <mouad.elb06@gmail.com>
2023-04-28 15:01:28 -0700

    add comment for Return

--- _printf.c
+++ _printf.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * _printf - print function
- * @format: format argument
+ * _printf - function that print any format
+ * @format: format argument %.
+ * Return: number of charcter
  */
 int _printf(const char *format, ...)
 {
